a1e74ec1ac869d57435253197416ee1d7bfaa0b6
Max Stauss <max.stauss@gmail.com>
2012-10-25 20:25:20 +0200

    reordering

--- _add-ons/form/pi.form.php
+++ _add-ons/form/pi.form.php
@@ -15,29 +15,54 @@ class Plugin_form extends Plugin {
     
   }
 
+  
 
-  private function _loginForm($return){
 
-    $html = "{{ member:login return='".$return."' }}";
-      $html .= '<input type="text" placeholder="Username" name="username" />';
-      $html .= '<input type="password" placeholder="Password" name="password" />';
-      $html .= '<input type="submit" class="button right" value="Login" />';
-    $html .= "{{ /member:login }}";
+  private function _loginForm($return='/', $title="Please Login"){
+
+    $html = '<form class="" method="post" action="/TRIGGER/member/login">';
+      $html .= '<input type="hidden" name="return" value="'.$return.'">';
+      $html .= '<header class="row">';
+        $html .= '<div class="twelve columns inline">';
+          $html .= '<h1>'.$title.'</h1>';
+          $html .= '<hr>';
+        $html .= '</div>';
+      $html .= '</header>';
+      $html .= '<div class="row">';
+        $html .= '<div class="three columns">';
+          $html .= '<label for="loginuser" class="">Username</label>';
+        $html .= '</div>';
+        $html .= '<div class="nine columns">';
+          $html .= '<input id="loginuser" type="text" placeholder="username" class="">';
+        $html .= '</div>';
+      $html .= '</div>';
+      $html .= '<div class="row">';
+        $html .= '<div class="three columns">';
+          $html .= '<label for="loginpass" class="">Password</label>';
+        $html .= '</div>';
+        $html .= '<div class="nine columns">';
+          $html .= '<input id="loginpass" type="text" placeholder="******">'; 
+        $html .= '</div>';
+      $html .= '</div>';
+      $html .= '<footer class="row">';
+        $html .= '<div class="twelve columns">';
+          $html .= '<input type="submit" class="button expand" value="Login">';
+        $html .= '</div>';
+      $html .= '</footer>';
+    $html .= '</form>';
 
     return $html;
   }
 
   public function modal_login(){
-    $title      = "Please Login";
+    
     $return     = $this->fetch_param('return', '{ page_url }');
 
 
     $btn = '<a href="#" data-reveal-id="loginModal" data-animation="fadeAndPop" data-animationspeed="300" data-closeonbackgroundclick="true" data-dismissmodalclass="close-reveal-modal">Login</a>';
 
     $div = '<div id="loginModal" class="reveal-modal small">';
-      $div .= '<h2>'.$title.'</h2>';
       $div .= $this->_loginForm($return);
-      $div .= '<a class="close-reveal-modal">&#215;</a>';
     $div .= "</div>";
 
     return array('button' => $btn, 'div' => $div);
